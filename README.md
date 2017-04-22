3450132afd7131499c1ba569e60c3de8a288fce0
Christian Kniep <christian@qnib.org>
2017-04-22 11:06:40 +0200

    update filter to send back and forward

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # qframe-filter-id
-qframe filter, which just passes input to the output
+qframe filter, which just passes input to the data or back channel
+
+
+## main.go
+
+When configured like this...
+
+```go
+cfgMap := map[string]string{
+    "filter.test.send-back": "docker-event",
+    "filter.test.send-data": "docker-event",
+    "filter.test.inputs": "docker-events",
+}
+```
+... an incoming `collector-docker-events` will be pushed to both channels.
+
+
+```bash
+$ go run main.go
+2017/04/22 09:03:16 [II] Dispatch broadcast for Back, Data and Tick
+2017/04/22 09:03:16 [II] Start id filter '%s' test
+2017/04/22 09:03:18 Send message
+#### Received message on Data-channel: docker-event
+#### Received message on Back-channel: docker-event
+```
+
+## Development
+
+```bash
+$ docker run -ti --name qframe-collector-docker-events --rm -e SKIP_ENTRYPOINTS=1 \
+           -v ${GOPATH}/src/github.com/qnib/qframe-filter-id:/usr/local/src/github.com/qnib/qframe-filter-id \
+           -v ${GOPATH}/src/github.com/qnib/qframe-types:/usr/local/src/github.com/qnib/qframe-types \
+           -v ${GOPATH}/src/github.com/qnib/qframe-utils:/usr/local/src/github.com/qnib/qframe-utils \
+           -w /usr/local/src/github.com/qnib/qframe-filter-id \
+            qnib/uplain-golang bash
+root@835291194a41:/usr/local/src/github.com/qnib/qframe-filter-id# govendor update github.com/qnib/qframe-types github.com/qnib/qframe-utils
+root@835291194a41:/usr/local/src/github.com/qnib/qframe-filter-id# govendor fetch +m
+```
--- main.go
+++ main.go
@@ -1,3 +1,69 @@
 package main
 
+import (
+	"log"
+	"fmt"
+	"time"
+	"github.com/zpatrick/go-config"
+	"github.com/qnib/qframe-types"
+	"github.com/qnib/qframe-utils"
+	"github.com/qnib/qframe-filter-id/lib"
+	"github.com/moby/moby/api/types/events"
+)
 
+
+func Run(qChan qtypes.QChan, cfg config.Config, name string) {
+	p := qframe_filter_id.New(qChan, cfg, name)
+	p.Run()
+}
+
+func main() {
+	myId := qutils.GetGID()
+	qChan := qtypes.NewQChan()
+	qChan.Broadcast()
+	cfgMap := map[string]string{
+		"filter.test.send-back": "docker-event",
+		"filter.test.send-data": "docker-event",
+		"filter.test.inputs": "docker-events",
+	}
+
+	cfg := config.NewConfig(
+		[]config.Provider{
+			config.NewStatic(cfgMap),
+		},
+	)
+	p := qframe_filter_id.New(qChan, *cfg, "test")
+	go p.Run()
+	time.Sleep(2*time.Second)
+	dc := qChan.Data.Join()
+	bc := qChan.Back.Join()
+	qm := qtypes.NewQMsg("collector", "docker-events")
+	qm.SourceID = myId
+	qm.Msg = "docker-event"
+	qm.Data = events.Message{}
+	log.Println("Send message")
+	qChan.Data.Send(qm)
+	gotData := false
+	gotBack := false
+	for {
+		select {
+		case msg := <-dc.Read:
+			qm := msg.(qtypes.QMsg)
+			if qm.SourceID == myId {
+				continue
+			}
+			fmt.Printf("#### Received message on Data-channel: %s\n", qm.Msg)
+			gotData = true
+		case msg := <-bc.Read:
+			qm := msg.(qtypes.QMsg)
+			if qm.SourceID == myId {
+				continue
+			}
+			fmt.Printf("#### Received message on Back-channel: %s\n", qm.Msg)
+			gotBack = true
+		}
+		if gotBack && gotData {
+			break
+		}
+	}
+}
