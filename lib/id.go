107a0b30cc1f86354085f11403989930e218234d
Christian Kniep <christian@qnib.org>
2017-04-22 11:06:40 +0200

    update filter to send back and forward

--- lib/id.go
+++ lib/id.go
@@ -5,28 +5,32 @@ import (
 	"log"
 	"strings"
 
+
 	"github.com/qnib/qframe-types"
 	"github.com/qnib/qframe-utils"
 	"github.com/zpatrick/go-config"
+	"github.com/moby/moby/api/types/events"
 )
 
 const (
 	version = "0.1.0"
+	pluginTyp = "filter"
 )
 
 type Plugin struct {
 	qtypes.Plugin
+	types []string
+	sendData []string
+	sendBack []string
 }
 
 func New(qChan qtypes.QChan, cfg config.Config, name string) Plugin {
 	p := Plugin{
-		Plugin: qtypes.Plugin{
-			QChan: qChan,
-			Cfg:   cfg,
-		},
+		Plugin: qtypes.NewNamedPlugin(qChan, cfg, pluginTyp, name, version),
+		types: []string{},
 	}
-	p.Version = version
-	p.Name = name
+	p.sendBack = strings.Split(p.CfgStringOr("send-back", ""), ",")
+	p.sendData = strings.Split(p.CfgStringOr("send-data", ""), ",")
 	return p
 }
 
@@ -35,6 +39,7 @@ func (p *Plugin) Run() {
 	log.Println("[II] Start id filter '%s'", p.Name)
 	myId := qutils.GetGID()
 	bg := p.QChan.Data.Join()
+	inputs := p.GetInputs()
 	for {
 		val := bg.Recv()
 		switch val.(type) {
@@ -43,13 +48,22 @@ func (p *Plugin) Run() {
 			if qm.SourceID == myId {
 				continue
 			}
-			if len(inputs) != 0 && !qutils.IsLastSource(inputs, qm.Source) {
+			if len(inputs) != 0 && !qutils.IsInput(inputs, qm.Source) {
 				continue
 			}
 			qm.Type = "filter"
 			qm.Source = p.Name
 			qm.SourceID = myId
-			p.QChan.Data.Send(qm)
+			switch qm.Data.(type) {
+			case events.Message:
+				if qutils.IsItem(p.sendData, "docker-event") {
+					p.QChan.Data.Send(qm)
+				}
+				if qutils.IsItem(p.sendBack, "docker-event") {
+					p.QChan.Back.Send(qm)
+				}
+			}
+
 		}
 	}
 }
